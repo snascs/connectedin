82028be518f0249532b9a8e3cb5ae8fa2cc8e3d1
unknown <samuel_ns@msn.com>
2015-09-16 20:35:35 -0300

    Alterações da aula 9 - Aceitando convites

--- perfis/views.py
+++ perfis/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from perfis.models import Perfil
+from perfis.models import Perfil, Convite
 from django.shortcuts import redirect
 
 def index(request):
@@ -9,7 +9,9 @@ def index(request):
 def exibir(request, perfil_id):
 	
 	perfil = Perfil.objects.get(id=perfil_id)
-	return render(request, 'perfil.html', { "perfil" : perfil, "perfil_logado" : get_perfil_logado(request)})
+	perfil_logado = get_perfil_logado(request)
+	ja_eh_contato = perfil in perfil_logado.contatos.all()
+	return render(request, 'perfil.html', { "perfil" : perfil, "perfil_logado" : get_perfil_logado(request), 'ja_eh_contato' : ja_eh_contato})
 
 def convidar(request, perfil_id):
 
@@ -18,6 +20,11 @@ def convidar(request, perfil_id):
 	perfil_logado.convidar(perfil_a_convidar)
 
 	return redirect('index');
+
+def aceitar(request, convite_id):
+	convite = Convite.objects.get(id=convite_id)
+	convite.aceitar()
+	return redirect('index')
 
 def get_perfil_logado(request):
 	return Perfil.objects.get(id=1) 
